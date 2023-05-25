691492370cc8b9441b7798e358e21450f1912027
DevHolako <holakonoob@gmail.com>
2023-05-25 15:44:36 +0100

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bulma/css/bulma.min.css";
 import "./styles/main.css";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { ToastContainer } from "react-toastify";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer />
+  </Provider>
+);
